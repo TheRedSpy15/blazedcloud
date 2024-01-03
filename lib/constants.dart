d596a73a2c2824f785c9b9448de19b961824cad3
Hunter Austin <hjadar15@protonmail.com>
2024-01-02 20:34:59 -0500

    switch back end url to prod

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,12 +4,12 @@ import 'package:pocketbase/pocketbase.dart';
 // localhost
 //const String backendUrl = "http://10.0.2.2:8090";
 // lan
-const String backendUrl = "http://192.168.68.89:8089";
+//const String backendUrl = "http://192.168.68.89:8089";
 // tests
 //const String backendUrl = "http://127.0.0.1:8090";
 // devs
 //const String backendUrl = "https://hazmav.tplinkdns.com:8089";
 // prod
-//const String backendUrl = "https://pb.blazedcloud.com";
+const String backendUrl = "https://pb.blazedcloud.com";
 
 var pb = PocketBase(backendUrl, authStore: CustomAuthStore());
